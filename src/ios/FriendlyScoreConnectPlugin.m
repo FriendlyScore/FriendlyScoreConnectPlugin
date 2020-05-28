21a379a27fba8e755dab51b07fb54d6da3e8f6c7
Lukasz Czechowicz <jestspoko@gmail.com>
2020-05-28 12:16:35 +0200

    added callbacks

--- src/ios/FriendlyScoreConnectPlugin.m
+++ src/ios/FriendlyScoreConnectPlugin.m
@@ -18,5 +18,18 @@ NSString *environment = @"production";
 {
     NSString* userRef = [command.arguments objectAtIndex:0];
     [FS connectWithClientId:clientId userReference:userRef environment:environment];
+    
+    FS.errorHandler = ^( NSString* error) {
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    };
+    FS.eventHandler = ^( NSString* event) {
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:event];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    };
+    
+    
 }
 @end
